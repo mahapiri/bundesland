5ea22b80751bea9004ae14e96f7c9467df1f4e19
Pirathib Mahalingam <pirathib.m@gmail.com>
2023-12-16 22:52:40 +0100

    added functions

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let website = [];
 
 async function init () {
     await loadCity();
+    loadCard();
 }
 
 
@@ -17,4 +18,39 @@ async function loadCity() {
         population.push(currentCity['population']);
         website.push(currentCity['url']);
     }
+}
+
+function loadCard() {
+    let content = document.getElementById('content');
+    
+    for (let i = 0; i < cityName.length; i++) {
+        content.innerHTML += /*html*/`
+            <tr class="card" id="card${i}" onmouseover="showLink(${i})">
+                <td class="card-info">
+                    <h4>${cityName[i]}</h4>
+                    <p>${population[i]} Millionen</p>
+                    <div class="link d-none" id="link${i}">
+                        <p>Link: <span onclick="window.open('${website[i]}', '_blank')">${website[i]}</span></p>
+                    </div> 
+                </td>
+            </tr>
+            
+    `;
+
+    
+    }    
+}
+
+function showLink(i) {
+    let link = document.getElementById(`link${i}`);
+    link.classList.remove('d-none');   
+}
+
+function clear(i) {
+    let link = document.getElementById(`link${i}`);
+    link.classList.add('d-none');
+}
+
+function timeOut(i) {
+    setTimeout(showLink(i), 1000);
 }
